d893952bfa27c26764e863f85998e06c4ed706d3
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-03-31 20:48:44 +0300

    Шаблон и разметка для одного блюда

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import dishTpl from './tamplates/dish.hbs';
+import menuData from './menu.json';
+// const menuJS = JSON.parse(menu);
+console.log(dishTpl);
+const menuList = document.querySelector('.js-menu');
+menuList.innerHTML = dishTpl(menuData[0]);
+// console.log(menuList);
+// console.log("dish");
