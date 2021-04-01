292af24d7077a45b3f89ea662a7a0dd01a91e554
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-04-01 22:53:40 +0300

    реализовано переключение тем

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import './styles.css';
 import dishTpl from './tamplates/dish.hbs';
 import menuData from './menu.json';
-// const menuJS = JSON.parse(menu);
-console.log(dishTpl);
+
+const body = document.querySelector("body");
+
 const menuList = document.querySelector('.js-menu');
 menuList.innerHTML = dishTpl(menuData[0]);
-// console.log(menuList);
-// console.log(createAllDishesCards);
+
 function createAllDishesCards(menuData) {
     menuList.innerHTML = menuData.map(dishTpl).join('');
 };
 createAllDishesCards(menuData);
-// menuList.innerHTML = createAllDishesCards;
+
+
+const checkBox = document.querySelector('#theme-switch-toggle');
+checkBox.addEventListener('change', toChangeTheme);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+
+const savedTheme = localStorage.getItem('Theme');
+
+function toChangeTheme(event) {    
+    body.classList.toggle(DARK);
+    if (body.classList.contains(DARK)) {
+         localStorage.setItem('Theme', DARK);
+        checkBox.setAttribute('checked', true);
+       
+    } else {
+       localStorage.setItem('Theme', LIGHT);
+        checkBox.setAttribute('checked', false);
+    }
+    
+}
+
+if (savedTheme === Theme.DARK) {
+    body.classList.add(DARK);
+     checkBox.setAttribute('checked', true);
+}
+
+
